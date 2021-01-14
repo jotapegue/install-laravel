472a47a7aaf5eda826b13f0373464359d784bab4
João Pedro Garcia <jotapegue09@gmail.com>
2021-01-14 10:36:14 -0300

    compability with mongo

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 sudo apt install php
-sudo apt install openssl php-common php-cli php-fpm php-curl php-json php-mbstring php-mysql php-xml php-zip
+sudo apt install openssl php-common php-cli php-fpm php-curl php-json php-mbstring php-mysql php-xml php-zip php-dev php-pear
 
 composer global require laravel/installer
 
 echo export PATH=$PATH:$HOME/.config/composer/vendor/bin >> .zshrc
+
+sudo pecl upgrade mongodb
+
+#add on php.ini
+echo "add on php.ini: extension=mongodb.so"
